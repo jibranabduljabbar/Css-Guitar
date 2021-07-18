5e21a99b8ee3b0745e6417125ee53c2f24166224
Jibran <jibran.jabbar06@gmail.com>
2021-07-18 19:11:24 +0500

    Guitar Using Css

---
+++ App.js
@@ -0,0 +1,6 @@
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play(){ document.getElementById("audio").play();
+}
+
+alert("Just click on the screen and feel the music 🎶🎶")
